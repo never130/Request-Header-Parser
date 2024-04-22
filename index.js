72bc753fe9438ed1ceb84eb414ab38cc52a46f80
Ever Loza Ruiz <never130@hotmail.com>
2024-04-22 03:37:47 +0000

    coreccion

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
   res.json({ 
-    ip_adress : req.socket.remoteAddress,
+    ipaddress : req.socket.remoteAddress,
     language: req.headers['aceppt-language'],
     software: req.header['user-agent']
    });

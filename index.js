ed644cfac96791fc6d9be6807fde3810d3c0235c
Ever Loza Ruiz <never130@hotmail.com>
2024-04-22 03:39:18 +0000

    1

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/whoami', function (req, res) {
   res.json({ 
     ipaddress : req.socket.remoteAddress,
     language: req.headers['aceppt-language'],
-    software: req.header['user-agent']
+    software: req.headers['user-agent']
    });
 });
 
